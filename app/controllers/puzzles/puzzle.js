127c8f8c6905ddef9f853e183c0b8f73d242236e
jjseymour <jjseymour@gmail.com>
2016-06-16 18:07:07 -0400

    definition parsed and on page

--- app/controllers/puzzles/puzzle.js
+++ app/controllers/puzzles/puzzle.js
@@ -4,18 +4,17 @@ export default Ember.Controller.extend({
   entries: [],
   actions: {
     getEntry(word){
-      debugger;
+      // debugger;
       // if we got here because we clicked the word from a definition string
       // collection, make sure we grab the word correctly to make api request
       this.store.queryRecord('entry', {filter: {word: word}}).then((entry) => {
         // Ember.Object.create(word: entry.get('word'), definition}
         let entries = this.get('entries')
+        var definition = entry.get('definition')
+        entry.set('definition', definition.split(" "))
         entries.pushObject(entry)
+        debugger
       })
-     //
-     //       self.renderTemplate('entries.userattempt', {outlet: "entries", model: entry})
-     //       // this.bacon.push(entry)
-     //     });
     }
   }
 });
--- app/routes/puzzles/puzzle.js
+++ app/routes/puzzles/puzzle.js
@@ -4,21 +4,5 @@ export default Ember.Route.extend({
   model(params){
     var puzzle = this.store.peekRecord('puzzle', params.puzzle_id);
     return puzzle;
-  },
-  // bacon: ["something"],
-  // actions: {
-  //   getEntry(word){
-  //     this.toggleProperty('cheese')
-  //     self = this
-  //     // this.currentModel.get('start_word')
-  //     this.store.queryRecord('entry', {filter: {word: word}}).then((entry) => {
-  //
-  //       self.renderTemplate('entries.userattempt', {outlet: "entries", model: entry})
-  //       // this.bacon.push(entry)
-  //     });
-  //   },
-  //   sendEntry(){
-  //     debugger
-  //   }
-  // }
+  }
 });
