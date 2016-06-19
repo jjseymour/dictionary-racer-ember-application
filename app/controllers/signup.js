4c34e206cbc1452cff2b573a1ac04a592353849d
jjseymour <jjseymour@gmail.com>
2016-06-19 14:04:14 -0400

    users can signup

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -4,15 +4,18 @@ export default Ember.Controller.extend({
  session: Ember.inject.service('session'),
  actions: {
    save(user){
-     user.save().then(()=>{
-       var credentials = {identification: user.get('email'), password: user.get('password')},
-         authenticator = 'authenticator:jwt';
+     user.save().then((user)=>{
+       var credentials = {identification: user.get('email'), password: user.get('password')};
+       var authenticator = 'authenticator:jwt';
 
        this.get('session').authenticate(authenticator, credentials).catch((reason)=>{
+         debugger
          this.set('errorMessage', reason.error || reason);
        });
      }).catch((adapterError) => {
+       debugger
        if (adapterError.errors) {
+         debugger
          this.set('errorMessage', adapterError.errors)
        }
      })
