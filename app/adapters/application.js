42c53467913781c92699f1ac077d133923148967
jjseymour <jjseymour@gmail.com>
2016-06-20 11:28:29 -0400

    config vars

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,9 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import config from '../config/environment';
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: "http://localhost:3000",
+  host: `${config.host}`,
   namespace: "api/v1",
   authorizer: 'authorizer:custom'
 });
