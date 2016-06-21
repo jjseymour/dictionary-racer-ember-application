6431d57852395184fd89f987ad9c6f0123633ad0
alexseth1124 <alexseth1124@gmail.com>
2016-06-21 11:01:47 -0400

    reset controller works

--- app/routes/puzzles.js
+++ app/routes/puzzles.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll('puzzle');
-  }
+  },
+  actions: {
+  clearPuzzle: function(puzzle) {
+    let controller = this.controllerFor('puzzles.puzzle')
+    controller.set('entries', [])
+    controller.set('colors', [])
+    controller.set('path', [])
+    controller.set('count', 0)
+    controller.set('showMyModal', false)
+    controller.set('bestPathBeaten', false)
+  }}
 });
